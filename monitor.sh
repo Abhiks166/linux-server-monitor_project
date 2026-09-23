adf5a1da035004d2d5477e65f097f66afef168ad
Abhiks166 <ksabhiadi1818@gmail.com>
2026-09-23 16:30:59 +0000

    Add load average monitoring

--- monitor.sh
+++ monitor.sh
@@ -2,6 +2,8 @@
 #!/bin/bash
 echo -e "============================================ \nLINUX SERVER MONITOR \n============================================  "
 echo -e "\n\n"
-echo -e "Hostname        : $(hostname) \nUser        : $(whoami) \nDate        ; $(date) \nSystem        :$(uname)"
+echo -e "Hostname        : $(hostname) \nUser            : $(whoami) \nDate            : $(date) \nSystem          : $(uname)"
+load_avg_1Min=$(cat /proc/loadavg | awk '{print $1}')
+echo -e "\n Load avg       : $load_avg_1Min"
 echo -e "\n============================================ "
 
