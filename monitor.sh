cc7a3b35f71c082b85d200dcc8714c5c92f8aec1
Abhi <ksabhiadi1818@gmail.com>
2026-09-23 21:12:18 +0530

    Initial code in monitor file

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,7 @@
 
 #!/bin/bash
+echo -e "============================================ \nLINUX SERVER MONITOR \n============================================  "
+echo -e "\n\n"
+echo -e "Hostname        : $(hostname) \nUser        : $(whoami) \nDate        ; $(date) \nSystem        :$(uname)"
+echo -e "\n============================================ "
+
